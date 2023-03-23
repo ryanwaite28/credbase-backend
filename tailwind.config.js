4d5f2e38dfffa5d0ab82a8ce1d99d169df821c16
Ryan Waite <ryanwaite28@gmail.com>
2023-03-22 21:17:11 -0400

    refactored libraries; added angular frontend config and ui app

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./src/frontends/**/*.{html,ts}",
+  ],
+  // content: ['./**/*.{html}', ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      ...colors,
+      // black: colors.black,
+      // white: colors.white,
+      // gray: colors.gray,
+      // emerald: colors.emerald,
+      // indigo: colors.indigo,
+      // teal: colors.teal,
+      // green: colors.green,
+      // violet: colors.violet,
+      // yellow: colors.yellow,
+      // red: colors.red,
+      // blue: colors.blue,
+      // slate: colors.slate,
+    },
+    extend: {
+      colors: {
+        primary: '#3b4252',
+        accent: '#9a8b74',
+
+        transparent: 'transparent',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        emerald: colors.emerald,
+        indigo: colors.indigo,
+        teal: colors.teal,
+        green: colors.green,
+        yellow: colors.yellow,
+        red: colors.red,
+        blue: colors.blue,
+        slate: colors.slate,
+      },
+    },
+  },
+  plugins: [
+    // require('flowbite/plugin')
+  ],
+}
